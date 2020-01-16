fb1ab830c9d0b7765953d9ad546db55a489f9fa3
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-16 21:18:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+  end
+ 
+  get '/hometown' do
+  "My hometown is Tallahassee"
+  end
+  
+  get '/name' do
+  "My name is Jayson"
+  end
+  
+  get '/favorite-song' do
+  "My favorite song is Back at One"
+  end
 end
