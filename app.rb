5e3f4b992de0feac65d2ca60fee406e2f74ce1bb
Jas Spencer <jas.spencer414@gmail.com>
2020-01-19 00:10:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Jas"
+  end
+
+  get '/hometown' do
+    "My hometown is Northfield"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is November Rain"
+  end
 end
